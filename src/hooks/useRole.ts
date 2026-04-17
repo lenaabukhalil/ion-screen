8b0002bae99635dcf984135cc800d6aaccde1a14
lenaabukhalil <lenabukhalil98@gmail.com>
2026-04-17 21:58:22 +0300

    update

--- src/hooks/useRole.ts
+++
@@ -1,7 +0,0 @@
-import { useAuth } from '@/context/AuthContext'
-
-export function useRole() {
-  const { user } = useAuth()
-  const isAdmin = Number(user?.organization_id) === 1
-  return { isAdmin }
-}
